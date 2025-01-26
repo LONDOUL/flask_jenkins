f0b3675b4d3b38a534f30a986f258b53a0eb7e82
NADJILEM LONDOUL Amédée <amedenadjilem@gmail.com>
2025-01-26 15:40:44 +0000

    Mon premier commit avec JenKins

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Jenkins with Flask!"
+
+@app.route('/test')
+def home():
+    return "Route de Test!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
